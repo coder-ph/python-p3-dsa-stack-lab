47b13d689812c283af6f0d605ad24be33f587bbd
Phelix <phelixmbani@gmail.com>
2024-12-24 11:41:57 +0300

    stack

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,38 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) ==0
+      
 
     def push(self, item):
-        pass
+        if self.full():
+        
+            raise OverflowError("stack is full")
+        else:
+            self.items.append(item)
 
     def pop(self):
-        pass
-
+        if self.isEmpty():
+            return None
+        return self.items.pop()
+        
+        
     def peek(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items[-1]
     
     def size(self):
-        pass
+       return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return len(self.items) -1 - self.items.index(target)
+        return -1
